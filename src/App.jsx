3a4b95053555d326fe6c758123379fa5d63ff550
Geoffrey <muchokigeoffreymunene@gmail.com>
2025-09-14 21:10:25 +0300

    Add react-use and debounce search input in App
    Added the 'react-use' package and implemented useDebounce in App.jsx to debounce the search term input by 500ms, reducing unnecessary API requests while typing. Updated dependencies in package.json.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Search from "./components/Search.jsx";
 import MovieCard from "./components/MovieCard.jsx";
+import { useDebounce } from "react-use";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -19,6 +20,11 @@ const App = () => {
     const [errorMessage, setErrorMessage] = useState('');
     const [movieList, setMovieList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+    // Debounce the search term to prevent making too many API requests
+    //by waiting for the user to stop typing for 500ms
+    useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
     
     const fetchMovies = async (query='') => {
         setIsLoading(true);
@@ -55,8 +61,8 @@ const App = () => {
     }
 
     useEffect(() => {
-        fetchMovies(searchTerm);
-    }, [searchTerm]);
+        fetchMovies(debouncedSearchTerm);
+    }, [debouncedSearchTerm]);
 
     return (
         <main>
